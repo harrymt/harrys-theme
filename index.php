9c4c364b7ebf09c5361ef632a11af724d5d01d21
Harry Mumford-Turner <harrymt@me.com>
2018-07-29 22:27:52 +0100

    Show the posts on the homepage

--- index.php
+++ index.php
@@ -9,6 +9,19 @@
   <h1>Hello Everyone</h1>
   <p>This is Harry's theme</p>
 
+  <?php
+  if ( have_posts() ) {
+    while ( have_posts() ) {
+      the_post();
+      ?>
+      <ul>
+        <li><?php the_title(); ?></li>
+      </ul>
+      <?php
+    } // end while
+  } // end if
+  ?>
+
   <?php get_footer(); ?>
 </body>
 </html>
